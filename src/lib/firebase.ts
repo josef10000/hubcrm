c3b6b9e2d76ee9fceef2272eaaa715d96db3f910
José Frazão <102549534+josef10000@users.noreply.github.com>
2026-03-10 10:48:51 -0300

    feat: Add file upload functionality
    Integrates Firebase Storage to allow users to upload and manage client-related files. This includes updates to the `Client` interface and imports for storage functionality.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBOzaAsS1MWLq6vU50PfOBD1xoIFflDa8E",
@@ -14,4 +15,5 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
 export const googleProvider = new GoogleAuthProvider();
