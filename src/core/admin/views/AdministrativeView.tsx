1b36563f62efcf66cdd43d974c0514b7ba473c81
José Frazão da Silva Neto <jfs102019@hotmail.com>
2026-06-05 14:41:25 -0300

    fix: define variavel i em formatBytes no AdministrativeView

--- src/core/admin/views/AdministrativeView.tsx
+++ src/core/admin/views/AdministrativeView.tsx
@@ -96,6 +96,7 @@ export default function AdministrativeView() {
     const k = 1024;
     const dm = decimals < 0 ? 0 : decimals;
     const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
     return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
   };
 
